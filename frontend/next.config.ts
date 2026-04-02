312fd2dde4e98c5a19959e15e34b250d32ccf936
Joaquin Vasquez <joaquinvcestudio@gmail.com>
2026-04-02 11:51:42 -0500

    fix: add rewrites proxy to backend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://129.159.179.42:8080/api/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
